eaacbc5360d91256a5eea381bce98658fb8b78a7
0mk4rm4li <54142632+0mk4rm4li@users.noreply.github.com>
2021-01-15 12:03:05 -0500

    Add files via upload

---
+++ scripts/tool.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+if [ $EUID != 0 ]
+then
+  echo "Please run this script as root so as to see all details! Better run with sudo."
+  exit 1
+fi
+
+green="\e[0;92m"
+reset="\e[0m"
+red="\e[0;91m"
+bold="\e[1m"
+blue="\e[0;94m"
+
+
+echo -e "${blue}
+  ________                    __       __  __            __
+ /_  __/ /_  ________  ____ _/ /_     / / / /_  ______  / /____  _____
+  / / / __ \/ ___/ _ \/ __ \/ __/    / /_/ / / / / __ \/ __/ _ \/ ___/
+ / / / / / / /  /  __/ /_/ / /_     / __  / /_/ / / / / /_/  __/ /
+/_/ /_/ /_/_/   \___/\__,_/\__/____/_/ /_/\__,_/_/ /_/\__/\___/_/ 
+                             /_____/ ${reset}
+
+				    ${red} - Made With <3 By Group_10.${reset}"
+
+
+echo -e "${green}
+
+ 1. SQL Injection
+ 2. Cross Site Scripting (XSS)
+ 3. Local File Inclusion (LFI)
+ 4. Log Analysis${reset}${red}
+ 5. Exit ${reset}"
+ echo ''
+read -p " Enter the respective number from list that you want to test: " number
+
+case $number in 
+	1)	
+		echo -e $"\n\n ${red} ========== Testing SQL Injection ========== ${reset}\n"
+		read -p "Enter The URL : " sql
+		/root/project_2/scripts/./sql.sh $sql
+		;;
+	
+	2)	
+		echo -e $"\n\n ${red} ========== Testing Cross Site Scripting (XSS) ========== ${reset}\n"
+		read -p "Enter the URL :" xss
+		/root/project_2/scripts/./xss.sh $xss
+		;;
+	
+	3)	
+		echo -e $"\n\n ${red} ========== Testing Local File Inclusion (LFI) ========== ${reset}\n"
+		read -p "Enter The URL : " lfi
+		/root/project_2/scripts/./lfi.sh $lfi
+		;;
+	
+	4)	
+		echo -e $"\n\n ${red} ========== Doing Log Analysis ========== ${reset}\n"
+		;;
+	
+	5)	
+		exit 1
+		;;
+	
+	*)
+		echo "Please give valid choice!!!"
+		;;
+	
+esac	
+
+
+
+
+
+
+
+
---
+++ scripts/xsshtml.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+if [ $EUID != 0 ]
+then
+  echo "Please run this script as root so as to see all details! Better run with sudo."
+  exit 1
+fi
+
+
+#Creating a directory if it doesn't exist to store reports first, for easy maintenance.
+
+
+if [ ! -d /home/noob/html_repo ]
+then
+  mkdir /home/noob/html_repo
+fi
+	html="/home/noob/html_repo/xss_report.html"
+	email_add="test411062@gmail.com"
+	#for i in `ls /home`; do sudo du -sh /home/$i/* | sort -nr | grep G; done > /tmp/dir.txt
+
+#Creating HTML Reports
+
+echo "<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">" >> $html
+echo "<html>" >> $html
+echo "<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css">" >> $html
+echo "<body>" >> $html
+echo "<fieldset>" >> $html
+echo "<center>" >> $html
+echo "<h2>XSS Report</h2>" >> $html
+echo "<h3><legend>Made With <3 by Group 10</legend></h3>" >> $html
+echo "</center>" >> $html
+echo "<!DOCTYPE html>" >> $html
+echo "<html>" >> $html
+echo "<body>" >> $html
+echo "<h3>[-] Vulnerable To XSS : </h3><br>" >> $html
+for i in $(cat $1); do
+echo "<p><a href="$i">$i</a></p><br>" >> $html ;done
+echo "</body>" >> $html
+
